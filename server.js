a3bca867ef26c4847f91f8301d4bb27bb0a9323a
Himanshu Pal <himanshu.p19@iiits.in>
2021-10-22 02:33:26 +0530

    adding database for pagination api

--- server.js
+++ server.js
@@ -1,50 +1,40 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-const users = [
-  { id: 1, name: "user 1" },
-  { id: 2, name: "user 2" },
-  { id: 3, name: "user 3" },
-  { id: 4, name: "user 4" },
-  { id: 5, name: "user 5" },
-  { id: 6, name: "user 6" },
-  { id: 7, name: "user 7" },
-  { id: 8, name: "user 8" },
-  { id: 9, name: "user 9" },
-  { id: 10, name: "user 10" },
-  { id: 11, name: "user 11" },
-  { id: 12, name: "user 12" },
-  { id: 13, name: "user 13" },
-  { id: 14, name: "user 14" },
-];
+const User = require("./users");
 
-const posts = [
-  { id: 1, name: "post 1" },
-  { id: 2, name: "post 2" },
-  { id: 3, name: "post 3" },
-  { id: 4, name: "post 4" },
-  { id: 5, name: "post 5" },
-  { id: 6, name: "post 6" },
-  { id: 7, name: "post 7" },
-  { id: 8, name: "post 8" },
-  { id: 9, name: "post 9" },
-  { id: 10, name: "post 10" },
-  { id: 11, name: "post 11" },
-  { id: 12, name: "post 12" },
-  { id: 13, name: "post 13" },
-  { id: 14, name: "post 14" },
-];
-
-app.get("/posts", paginatedResult(posts), (req, res) => {
-  res.send(res.paginatedResult);
+mongoose.connect("mongodb://localhost/pagination", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get("/users", paginatedResult(users), (req, res) => {
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await User.countDocuments()) > 0) return; // we do not want to populate the database with the dulplicate users, if it has already contained some users in it.
+
+  // if there are no user, then we will create some in the database
+
+  Promise.all([
+    User.create({ name: "User 1" }),
+    User.create({ name: "User 2" }),
+    User.create({ name: "User 3" }),
+    User.create({ name: "User 4" }),
+    User.create({ name: "User 5" }),
+    User.create({ name: "User 6" }),
+    User.create({ name: "User 7" }),
+    User.create({ name: "User 8" }),
+    User.create({ name: "User 9" }),
+    User.create({ name: "User 10" }),
+  ]).then(console.log("Added users"));
+});
+
+app.get("/users", paginatedResult(User), (req, res) => {
   res.json(res.paginatedResult);
 });
 
 function paginatedResult(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
 
@@ -54,13 +44,17 @@ function paginatedResult(model) {
     const results = {};
 
     if (startIndex > 0) results.prev = { page: page - 1, limit: limit };
-    if (endIndex < model.length)
+    if (endIndex < (await model.countDocuments().exec()))
       results.next = { page: page + 1, limit: limit };
 
-    results.result = model.slice(startIndex, endIndex);
-
-    res.paginatedResult = results;
-    next();
+    // results.result = model.slice(startIndex, endIndex);
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResult = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
   };
 }
 
